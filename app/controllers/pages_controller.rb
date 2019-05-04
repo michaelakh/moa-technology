8b4436c64d3ad54d967fe437ed40481d53019aa3
michaelakh <michael_ak@hotmail.co.uk>
2019-05-04 16:26:01 +0100

    precompile assets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,9 @@ class PagesController < ApplicationController
   def privacy
     @title = 'Privacy Policy | MOA Technologies'
   end
+  def cv
+    @cv = 'CV | Michael Ohioze Akhazemea | MOA Technologies'
+    send_file("#{Rails.root}/app/assets/docs/Michael_Akhazemea_CV_2019.pdf", {filename:'Michael Akhazemea CV 2019'})
+  end
   
 end
